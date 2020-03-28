a04c4d500f9307d6e1768ddc1f38966b06ef80e2
Ulan <kaltatay@gmail.com>
2020-03-28 15:33:54 -0400

    "Monthly budget. task 1"

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 'use strict';
 
-alert(1);
-//console.log(leftBorderWidth);
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-console.log(leftBorderWidth);
 //alert(1);
-let person = {
-    name: "Kevin",
-    age: 25
-}
-console.log(person);
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(leftBorderWidth);
+// alert(1);
+// let person1 = {
+//     name: "Kevin",
+//     age: 25
+// };
+// console.log(person1);
+// var age =25;
+// console.log(age);
+
+let money;
+let time;
+money = prompt("Ваш бюджет на месяц?");
+console.log(money);
+time = prompt("Введите дату в формате YYYY-MM-DD");
+let necessaryExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+let necessaryExpensesAmount = prompt("Во сколько обойдется?");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        necessaryExpenses: necessaryExpensesAmount
+
+    },
+    optionalExpenses:"",
+    income:[],
+    savings: false
+
+};
+alert(1);
+alert("бюджет на 1 день : " +money/30);
+
