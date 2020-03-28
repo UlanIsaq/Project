f515c1db6203f29f3b9a5827e5ab9676a2f11a81
Ulan <kaltatay@gmail.com>
2020-03-28 13:52:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+alert(1);
+//console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(leftBorderWidth);
+//alert(1);
+let person = {
+    name: "Kevin",
+    age: 25
+}
+console.log(person);
