475025668b519b797bced346d6ffa97627e09c6e
Ulan <kaltatay@gmail.com>
2020-03-28 21:13:55 -0400

    "loop task"

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 //alert(1);
 // console.log(leftBorderWidth);
 // var leftBorderWidth = 1;
@@ -14,29 +16,60 @@
 // console.log(person1);
 // var age =25;
 // console.log(age);
-
+////
+alert(1);
 let money;
 let time;
-money = prompt("Ваш бюджет на месяц?");
+money = +prompt("Ваш бюджет на месяц?");
 console.log(money);
 time = prompt("Введите дату в формате YYYY-MM-DD");
-let necessaryExpenses1 = prompt("Введите обязательную статью расходов в этом месяце");
-let necessaryExpensesAmount2 = prompt("Во сколько обойдется?");
-let necessaryExpenses2 = prompt("Введите обязательную статью расходов в этом месяце");
-let necessaryExpensesAmount3 = prompt("Во сколько обойдется?");
+let necessaryExpenses;
+let necessaryExpensesAmount;
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-    },
-    optionalExpenses:{},
-    income:[],
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
 
 };
-appData.expenses.necessaryExpenses1=necessaryExpensesAmount2;
-appData.expenses.necessaryExpenses2=necessaryExpensesAmount3;
 
-//alert(1);
-alert("бюджет на 1 день : " +appData.budget/30);
+// for (let i = 0; i < 2; i++) {
+//     necessaryExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+//     necessaryExpensesAmount = prompt("Во сколько обойдется?");
+//     if (typeof (necessaryExpenses) === 'string' && typeof (necessaryExpenses) != null && typeof (necessaryExpensesAmount) != null && necessaryExpenses != "" && necessaryExpensesAmount != "" && necessaryExpenses.length < 50 && typeof (necessaryExpensesAmount) === 'number') {
+//         appData.expenses[necessaryExpenses] = necessaryExpensesAmount;
+//         console.log('done');
+//     } else {
+//         console.log("Its not valid input info");
+//     }
+// }
+let k=0;
+while(k<2){
+    necessaryExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+    necessaryExpensesAmount = prompt("Во сколько обойдется?");
+    if (typeof (necessaryExpenses) === 'string' && typeof (necessaryExpenses) != null && typeof (necessaryExpensesAmount) != null && necessaryExpenses != "" && necessaryExpensesAmount != "" && necessaryExpenses.length < 50 && typeof (necessaryExpensesAmount) === 'number') {
+        appData.expenses[necessaryExpenses] = necessaryExpensesAmount;
+        console.log('done');
+    } else {
+        console.log("Its not valid input info");
+    }
+    k++;
+}
+
+
+appData.monerPerDay = appData.budget / 30;
+alert("бюджет на 1 день : " + appData.monerPerDay);
+
+if(necessaryExpensesAmount <= 100){
+    console.log('Min budget');
+
+}else if(necessaryExpensesAmount > 100 && necessaryExpensesAmount <=200){
+    console.log('Middle budget');
+    }else if(necessaryExpensesAmount > 200){
+        console.log('High budget');
+    }else {
+        console.log("smth wrong");
+    }
 
